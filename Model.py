04a9343bd9acb58ab67ee187d7db369dbf21552a
Satbek Abdyldayev <satbekmyrza@gmail.com>
2022-09-27 05:24:16 +0000

    reformat

--- Model.py
+++ Model.py
@@ -1,43 +1,43 @@
+from decodeTrans import decodeTransformerBlock
+from Embedding import Embedding
+from gcnnnormal import GCNNM
+from gelu import GELU
+from graphTransformer import graphTransformerBlock
+from LayerNorm import LayerNorm
+from Multihead_Attention import MultiHeadedAttention
+from Multihead_Combination import MultiHeadedCombination
+from postionEmbedding import PositionalEmbedding
+from rightnTransfomer import rightTransformerBlock
+from Transfomer import TransformerBlock
+from TreeConvGen import TreeConvGen
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch
-from Transfomer import TransformerBlock
-from rightnTransfomer import rightTransformerBlock
-from Multihead_Combination import MultiHeadedCombination
-from Embedding import Embedding
-from TreeConvGen import TreeConvGen
-from Multihead_Attention import MultiHeadedAttention
-from gelu import GELU
-from LayerNorm import LayerNorm
-from decodeTrans import decodeTransformerBlock
-from gcnnnormal import GCNNM
-from postionEmbedding import PositionalEmbedding
-from graphTransformer import graphTransformerBlock
 
 
 class TreeAttEncoder(nn.Module):
     def __init__(self, args):
         super(TreeAttEncoder, self).__init__()
+
         self.embedding_size = args.embedding_size
         self.nl_len = args.NlLen
         self.word_len = args.WoLen
+
         self.char_embedding = nn.Embedding(args.Vocsize, self.embedding_size)
-        self.token_embedding = Embedding(
-            args.Code_Vocsize, self.embedding_size)
+        self.token_embedding = Embedding(args.Code_Vocsize, self.embedding_size)
+
         self.feed_forward_hidden = 4 * self.embedding_size
-        self.conv = nn.Conv2d(self.embedding_size,
-                              self.embedding_size, (1, self.word_len))
-        self.transformerBlocks = nn.ModuleList(
-            [TransformerBlock(self.embedding_size, 8, self.feed_forward_hidden, 0.1) for _ in range(3)])
-        self.transformerBlocksTree = nn.ModuleList(
-            [TransformerBlock(self.embedding_size, 8, self.feed_forward_hidden, 0.1) for _ in range(3)])
+
+        self.conv = nn.Conv2d(self.embedding_size, self.embedding_size, (1, self.word_len))
+        self.transformerBlocks = nn.ModuleList([TransformerBlock(self.embedding_size, 8, self.feed_forward_hidden, 0.1) for _ in range(3)])
+        self.transformerBlocksTree = nn.ModuleList([TransformerBlock(self.embedding_size, 8, self.feed_forward_hidden, 0.1) for _ in range(3)])
 
     def forward(self, input_code, input_codechar, inputAd):
         codemask = torch.gt(input_code, 0)
         charEm = self.char_embedding(input_codechar)
         charEm = self.conv(charEm.permute(0, 3, 1, 2))
         charEm = charEm.permute(0, 2, 3, 1).squeeze(dim=-2)
-        # print(charEm.shape)
         x = self.token_embedding(input_code.long())
         for trans in self.transformerBlocksTree:
             x = trans.forward(x, codemask, charEm, inputAd, True)
@@ -93,31 +93,32 @@ class CopyNet(nn.Module):
 
 class Decoder(nn.Module):
     def __init__(self, args):
+
         super(Decoder, self).__init__()
+
         self.embedding_size = args.embedding_size
         self.word_len = args.WoLen
         self.nl_len = args.NlLen
         self.code_len = args.CodeLen
         self.feed_forward_hidden = 4 * self.embedding_size
-        self.conv = nn.Conv2d(self.embedding_size,
-                              self.embedding_size, (1, args.WoLen))
-        self.path_conv = nn.Conv2d(
-            self.embedding_size, self.embedding_size, (1, 10))
-        self.rule_conv = nn.Conv2d(
-            self.embedding_size, self.embedding_size, (1, 2))
-        self.depth_conv = nn.Conv2d(
-            self.embedding_size, self.embedding_size, (1, 40))
+
+        self.conv = nn.Conv2d(self.embedding_size, self.embedding_size, (1, args.WoLen))
+        self.path_conv = nn.Conv2d(self.embedding_size, self.embedding_size, (1, 10))
+        self.rule_conv = nn.Conv2d(self.embedding_size, self.embedding_size, (1, 2))
+        self.depth_conv = nn.Conv2d(self.embedding_size, self.embedding_size, (1, 40))
+
         self.cnum = args.cnum
         self.resLen = args.rulenum - args.NlLen - self.cnum
-        self.encodeTransformerBlock = nn.ModuleList(
-            [rightTransformerBlock(self.embedding_size, 8, self.feed_forward_hidden, 0.1) for _ in range(9)])
-        self.decodeTransformerBlocksP = nn.ModuleList(
-            [decodeTransformerBlock(self.embedding_size, 8, self.feed_forward_hidden, 0.1) for _ in range(2)])
+
+        self.encodeTransformerBlock = nn.ModuleList([rightTransformerBlock(self.embedding_size, 8, self.feed_forward_hidden, 0.1) for _ in range(9)])
+        self.decodeTransformerBlocksP = nn.ModuleList([decodeTransformerBlock(self.embedding_size, 8, self.feed_forward_hidden, 0.1) for _ in range(2)])
+
         self.finalLinear = nn.Linear(self.embedding_size, 2048)
         self.resLinear = nn.Linear(2048, self.resLen)
-        self.rule_token_embedding = nn.Embedding(
-            args.Code_Vocsize, self.embedding_size)
+
+        self.rule_token_embedding = nn.Embedding(args.Code_Vocsize, self.embedding_size)
         self.rule_embedding = nn.Embedding(args.rulenum, self.embedding_size)
+
         self.encoder = NlEncoder(args)
         self.layernorm = LayerNorm(self.embedding_size)
         self.activate = GELU()
@@ -125,9 +126,9 @@ class Decoder(nn.Module):
         self.copy2 = CopyNet(args)
         self.copy3 = CopyNet(args)
         self.dropout = nn.Dropout(p=0.1)
-        self.depthembedding = nn.Embedding(
-            40, self.embedding_size, padding_idx=0)
+        self.depthembedding = nn.Embedding(40, self.embedding_size, padding_idx=0)
         self.char_embedding = nn.Embedding(args.Vocsize, self.embedding_size)
+
         self.gcnnm = GCNNM(self.embedding_size)
         self.position = PositionalEmbedding(self.embedding_size)
 
@@ -138,27 +139,30 @@ class Decoder(nn.Module):
 
     def forward(self, inputnl, inputnlad, inputrule, inputruleparent, inputrulechild, inputParent, inputParentPath, inputdepth, inputcodechar, tmpf, tmpc, tmpindex, tmpchar, tmpindex2, rulead, antimask, inputRes=None, mode="train"):
         selfmask = antimask
-        #selfmask = antimask.unsqueeze(0).repeat(inputtype.size(0), 1, 1).unsqueeze(1)
+
+        # selfmask = antimask.unsqueeze(0).repeat(inputtype.size(0), 1, 1).unsqueeze(1)
         # .unsqueeze(0).repeat(inputtype.size(0), 1, 1).float()
+
         admask = torch.eq(inputdepth, 1)
         rulemask = torch.gt(inputrule, 0)
         inputParent = inputParent.float()
         inputnlad = inputnlad.float()
+
         # encode_token
         charEm = self.char_embedding(tmpchar.long())
         charEm = self.conv(charEm.permute(0, 3, 1, 2))
         charEm = charEm.permute(0, 2, 3, 1).squeeze(dim=-2)
         rule_token_embedding = self.rule_token_embedding(tmpindex2[0])
         rule_token_embedding = rule_token_embedding + charEm[0]
+
         # encode_nl
-        # print(rule_token_embedding.size())
         nlencoding = F.embedding(inputnl.long(), rule_token_embedding)
         charEm = self.char_embedding(inputcodechar.long())
         charEm = self.conv(charEm.permute(0, 3, 1, 2))
         charEm = charEm.permute(0, 2, 3, 1).squeeze(dim=-2)
         nlencoding += self.position(inputnl)
-        nlencode, nlmask = self.encoder(
-            nlencoding, inputnlad, inputnl, inputdepth, charEm)
+        nlencode, nlmask = self.encoder(nlencoding, inputnlad, inputnl, inputdepth, charEm)
+
         # encode_rule
         # self.rule_token_embedding(tmpc)
         childEm = F.embedding(tmpc, rule_token_embedding)
@@ -167,16 +171,13 @@ class Decoder(nn.Module):
         childEm = self.layernorm(childEm)
         # self.rule_token_embedding(tmpf)
         fatherEm = F.embedding(tmpf, rule_token_embedding)
-        ruleEmCom = self.rule_conv(torch.stack(
-            [fatherEm, childEm], dim=-2).permute(0, 3, 1, 2))
-        ruleEmCom = self.layernorm(
-            ruleEmCom.permute(0, 2, 3, 1).squeeze(dim=-2))
+        ruleEmCom = self.rule_conv(torch.stack([fatherEm, childEm], dim=-2).permute(0, 3, 1, 2))
+        ruleEmCom = self.layernorm(ruleEmCom.permute(0, 2, 3, 1).squeeze(dim=-2))
         x = self.rule_embedding(tmpindex[0])
         rulenoter = x[:self.cnum]
         ruleter = x[self.cnum:]
         for i in range(9):
-            rulenoter = self.gcnnm(rulenoter, rulead[0], ruleEmCom[0]).view(
-                self.cnum, self.embedding_size)
+            rulenoter = self.gcnnm(rulenoter, rulead[0], ruleEmCom[0]).view(self.cnum, self.embedding_size)
         ruleselect = torch.cat([rulenoter, ruleter], dim=0)
         # self.rule_embedding(inputrule)
         ruleEm = F.embedding(inputrule, ruleselect)
@@ -187,9 +188,9 @@ class Decoder(nn.Module):
         ppathEm = self.layernorm(ppathEm)
         x = self.dropout(ruleEm + self.position(inputrule))
         for trans in self.encodeTransformerBlock:
-            x = trans(x, selfmask, nlencode, nlmask,
-                      ppathEm, inputParent, admask)
+            x = trans(x, selfmask, nlencode, nlmask, ppathEm, inputParent, admask)
         decode = x
+
         # ppath
         # self.rule_token_embedding(inputParentPath)
         Ppath = F.embedding(inputParentPath, rule_token_embedding)
@@ -216,33 +217,34 @@ class Decoder(nn.Module):
         res2 = res2 * prob[:, :, 1].unsqueeze(-1)
         res3 = res3 * prob[:, :, 2].unsqueeze(-1)
         res4 = res4 * prob[:, :, 3].unsqueeze(-1)
-        #genP = F.softmax(genP, dim=-1)
+        # genP = F.softmax(genP, dim=-1)
 
-        #x = self.finalLinear(decode)
-        #x = self.activate(x)
-        #x = self.resLinear(x)
-        #resSoftmax = F.softmax(x, dim=-1)
+        # x = self.finalLinear(decode)
+        # x = self.activate(x)
+        # x = self.resLinear(x)
+        # resSoftmax = F.softmax(x, dim=-1)
 
-        #resSoftmax = resSoftmax * prob[:,:,0].unsqueeze(-1)
-        #genP = genP * prob[:,:,1].unsqueeze(-1)
+        # resSoftmax = resSoftmax * prob[:,:,0].unsqueeze(-1)
+        # genP = genP * prob[:,:,1].unsqueeze(-1)
         # F.softmax(genP, dim=-1)#torch.cat([resSoftmax, genP], -1)
+
         resSoftmax = torch.cat([res1, res3, res2, res4], dim=-1)
+
         if mode != "train":
             return resSoftmax
+
         resmask = torch.gt(inputRes, 0)
-        #print(torch.gather(resSoftmax, -1, inputRes.unsqueeze(-1)).squeeze(-1))
-        loss = -torch.log(torch.gather(resSoftmax, -1,
-                          inputRes.unsqueeze(-1)).squeeze(-1))
-        # print(loss[7].data.cpu().numpy())
+        loss = -torch.log(torch.gather(resSoftmax, -1, inputRes.unsqueeze(-1)).squeeze(-1))
         loss = loss.masked_fill(resmask == 0, 0.0)
         resTruelen = torch.sum(resmask, dim=-1).float()
         totalloss = torch.mean(loss, dim=-1) * self.code_len / resTruelen
         # + (self.getBleu(loss, 2) + self.getBleu(loss, 3) + self.getBleu(loss, 4)) / resTruelen
         totalloss = totalloss
-        #totalloss = torch.mean(totalloss)
+        # totalloss = torch.mean(totalloss)
         return totalloss, resSoftmax
 
 
+# this model is not used anywhere
 class JointEmbber(nn.Module):
     def __init__(self, args):
         super(JointEmbber, self).__init__()
