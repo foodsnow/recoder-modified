0ad870c95f1fd3bd0a5452b74a831e0a4e6fcf8c
Satbek Abdyldayev <satbekmyrza@gmail.com>
2022-09-27 20:34:37 +0000

    refactor rename vars

--- testDefect4j.py
+++ testDefect4j.py
@@ -667,9 +667,9 @@ for i, project_name in enumerate(PROJECTS_V1_2):
             tmp_tree_root = convert_AST_as_list_to_tree(ast_as_list)
 
             # current node by line number
-            current_root = get_node_by_line_number(tmp_tree_root, buggy_line_number)
+            current_root_at_buggy_line = get_node_by_line_number(tmp_tree_root, buggy_line_number)
             # Subroot from current_root
-            lnode, mnode = getSubroot(current_root)
+            lnode, mnode = get_subroot(current_root_at_buggy_line)
             if mnode is None:
                 continue
 
