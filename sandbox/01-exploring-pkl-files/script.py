ac07d60aa90e264b948eff85950d35abdf8035b7
Satbek Abdyldayev <satbekmyrza@gmail.com>
2022-09-29 01:06:24 +0000

    add script

---
+++ sandbox/01-exploring-pkl-files/script.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import os
+import pickle, json
+import numpy as np
+
+
+cwd = Path(os.getcwd())
+first_n = 1
+
+for pklfile in cwd.glob('*.pkl'):
+    print(pklfile)
+    fname = pklfile.name[:-4]
+    pkldata = pickle.load(open(pklfile, 'rb'))
+    with open(fname + '.json', 'w') as fout:
+        if isinstance(pkldata, dict):
+            sample = {k: pkldata[k] for k in list(pkldata.keys())[:first_n]}
+        elif isinstance(pkldata, list):
+            sample = pkldata[:first_n]
+        elif isinstance(pkldata, np.ndarray):
+            sample = pkldata.tolist()
+        else:
+            assert 0
+        fout.write(json.dumps(sample))
